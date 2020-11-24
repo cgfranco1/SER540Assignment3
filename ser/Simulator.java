8ab7087907b155947885a4337207a00c9e67e03d
unknown <jonzerby@gmail.com>
2020-11-24 15:14:02 -0700

    got rid of some uneeded files and fix the gui

--- ser/Simulator.java
+++ ser/Simulator.java
@@ -2,15 +2,65 @@
  * all of the data has been read in, this class will simulate reading the data
  * in at real time. 
  */
-public class Simulator {
+import java.io.IOException;
+import javax.swing.*;
+
+import java.awt.event.*;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+public class Simulator implements ActionListener{
 
     private CANmessage[] msgArr;
     private int pointer;
     private String[] canValues;
-
+    public JTextArea textArea;
+    JLabel Label1;
     public Simulator(CANmessage[] msgArr) {
+        JFrame f=new JFrame();//creating JFrame   
+        JButton b=new JButton("Start/reset");//creating JButton 
+        Label1=new JLabel();  // new Jlabel 
+        Label1.setBounds(40,25,100,30); // bounds for the label 
+        Label1.setText("Simulator"); // setting text for label 
+        b.setBounds(150,550,120,30);//x axis, y axis, width, height  
+        b.addActionListener(this);  // adding action listener   
+        textArea = new JTextArea();  // new text area
+        //textArea.setBounds(5,65,525,450);
+        JScrollPane scrollbar=new JScrollPane(textArea);
+        scrollbar.setBounds(5,65,650,450);
+        scrollbar.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        f.add(Label1);  // adding stuff to the frame
+        f.add(b);
+        f.getContentPane().add(scrollbar);
+        f.setSize(700,700);//400 width and 500 height 
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit on close ability 
+        f.setLayout(null);//using no layout managers  
+        f.setVisible(true);//making the frame visible  
+        //PrintStream out = new PrintStream( new CustomOutputStream(textArea) ); // redirecting console output to gui to run simluation
+        //System.setOut(out); // system output
+        //System.setErr(out); // error output
         //timeOffset = 0.0;
         this.msgArr = msgArr;
+        pointer = 0;
+        canValues = new String [7];
+        //Represents steering angle.
+        canValues[0] = "-";
+        //Represents vehicle speed.
+        canValues[1] = "-";
+        //Represents yaw rate.
+        canValues[2] = "-";
+        //Represents lateral acceleration.
+        canValues[3] = "-";
+        //Represents longitudinal acceleration.
+        canValues[4] = "-";
+        //Represents GPS latitude.
+        canValues[5] = "-";
+        //Represents GPS longitude.
+        canValues[6] = "-";
+        
+    }
+    
+    public void actionPerformed(ActionEvent e){  
         pointer = 0;
         canValues = new String [7];
         //Represents steering angle.
@@ -30,6 +80,7 @@ public class Simulator {
     }
 
     public void startSimulation() {
+       
 
         //Represents the starting time of the simulation.
         long startTime = System.nanoTime();
@@ -40,13 +91,10 @@ public class Simulator {
         //Stringbuilder used to write out the CAN values.
         StringBuilder sb;
 
-        //Printing header.
-	    System.out.println("Current Time | Steering Angle | Vehicle Speed |" + 
-                           "    Yaw Rate    |  Lateral Acc  |" +
-                           " Longitudinal Acc | GPS Lat, Long");
-
+        
         //Loop until the end of CAN values is reached.
         while (pointer < msgArr.length) {
+            //textArea.setText(null);
             //Creating new StringBuilder.
             sb = new StringBuilder();
 
@@ -101,6 +149,9 @@ public class Simulator {
             }
 
             //Appending all CAN values to the StringBuilder.
+            sb.append("Current Time | Steering Angle | Vehicle Speed |" + 
+                           "    Yaw Rate    |  Lateral Acc  |" +
+                           " Longitudinal Acc | GPS Lat, Long\n");
             sb.append(String.format("%9.1f ms |", timeOffset));
             sb.append(String.format("%11s deg |", canValues[0]));
             sb.append(String.format("%9s km/h |", canValues[1]));
@@ -108,9 +159,10 @@ public class Simulator {
             sb.append(String.format("%8s m/s^2 |", canValues[3]));
             sb.append(String.format("%11s m/s^2 |", canValues[4]));
             sb.append(String.format(" (%s, %s)", canValues[5], canValues[6]));
-            sb.append("\r");
+            //sb.append("\r");
             //Printing out the CAN values.
-            System.out.print(sb.toString());
+            
+            textArea.setText(sb.toString());
 
             //Getting current system time.
             currentTime = System.nanoTime();
